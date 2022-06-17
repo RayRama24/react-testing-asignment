29233c4c51304501d2b3889334635f9c10cec88f
RayRama24 <raihanramadhann@gmail.com>
2022-06-17 17:39:35 +0700

    asignment 40

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CustomInput from "./CustomInput";
+import CatFact from "./CatFact";
+import AsignInput from "./AsignInput";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img id="image-react" data-testid="image-react" src={logo} className="App-logo" alt="logo" />
+        <p data-testid="App-text">
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a data-testid="App-link" className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <AsignInput />
+
+        <CustomInput />
+        <CatFact />
       </header>
     </div>
   );
